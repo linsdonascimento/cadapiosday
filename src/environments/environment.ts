b601b6e7747173a3c10b2ec86556611272ab903a
Thiago <linsdonascimento@gmail.com>
2019-09-10 10:59:17 -0300

    Aplicativo mobile de cardapio inicio

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyALZbzEdzjhuFBpvGdRawezwlDEvOS5lHo',
+    authDomain: 'db-carday.firebaseapp.com',
+    databaseURL: 'https://db-carday.firebaseio.com',
+    projectId: 'db-carday',
+    storageBucket: 'db-carday.appspot.com',
+    messagingSenderId: '16341400348',
+    appId: '1:16341400348:web:993ed01de0c42696'
+  }
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
